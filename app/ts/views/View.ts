66a558fe9bed1f262999a101da5bf5272033018c
SavioChermont <saviochwt@gmail.com>
2020-12-15 17:23:19 -0300

    Adiciona escape para evitar tag script do método de template

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,10 +1,17 @@
 export default abstract class View<T> {
   protected _elemento: JQuery;
-  constructor(seletor: string) {
+  private _escapar: boolean;
+
+  constructor(seletor: string, escapar: boolean = false) {
     this._elemento = $(seletor);
+    this._escapar = escapar;
   }
 
   update(model: T): void {
+    let template = this.template(model);
+    if (this._escapar) {
+      template = template.replace(/<script>[\s\S]*?<\/script>/, "");
+    }
     this._elemento.html(this.template(model));
   }
 
